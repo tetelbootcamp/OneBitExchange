04944cf2c56d8897396804886125467063657cde
tetel.player@gmail.com <tetel.player@gmail.com>
2020-10-06 11:39:04 -0300

    arrumando indentacao

--- app/services/exchange_service.rb
+++ app/services/exchange_service.rb
@@ -3,30 +3,28 @@ require 'json'
 
 class ExchangeService
 
-  def initialize(source_currency, target_currency, amount)
-  	@source_currency = source_currency
-  	@target_currency = target_currency
-  	@amount          = amount
-  end
+    def initialize(source_currency, target_currency, amount)
+  	  @source_currency = source_currency
+  	  @target_currency = target_currency
+  	  @amount          = amount
+    end
 
-  def call
-  	begin
-  	  value = get_exchange
-  	  value * @amount.to_f
-  	rescue RestClient::ExceptionWithResponse => e
-  		e.response
-  	end
-  end
+    def call
+  	  begin
+  	    value = get_exchange
+  	    value * @amount.to_f
+  	  rescue RestClient::ExceptionWithResponse => e
+  		  e.response
+  	  end
+    end
 
-  private
+    private
+    def get_exchange
+  	  exchange_api_url = Rails.application.credentials[:currency_api_url]
+  	  exchange_api_key = Rails.application.credentials[:currency_api_key]
+  	  url = "#{exchange_api_url}?token=#{exchange_api_key}&currency=#{@source_currency}/#{@target_currency}"
 
-  def get_exchange
-  	exchange_api_url = Rails.application.credentials[:currency_api_url]
-  	exchange_api_key = Rails.application.credentials[:currency_api_key]
-  	url = "#{exchange_api_url}?token=#{exchange_api_key}
-  	         &currency=#{@source_currency}/#{@target_currency}"
-
-    res = RestClient.get url
-    JSON.parse(res.body)['currency'][0]['value'].to_f   
-  end
+      res = RestClient.get url
+      JSON.parse(res.body)['currency'][0]['value'].to_f   
+    end
 end
