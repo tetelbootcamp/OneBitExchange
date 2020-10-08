546bbeed1ee3c827450efdfcba35f5eb6d0e2d40
tetel.player@gmail.com <tetel.player@gmail.com>
2020-10-08 16:33:37 -0300

    test deploy

--- app/javascript/packs/exchanges.js
+++ app/javascript/packs/exchanges.js
@@ -4,3 +4,28 @@ document.addEventListener('turbolinks:load', function(){
     document.getElementById('result').value = result.value
   })
 })
+
+
+/*document.addEventListener('keydown', function (event) {
+  document.getElementById('exchange_form').addEventListener('ajax:success', function(event) {
+    let [result] = event.detail
+    document.getElementById('result').value = result.value
+  })
+})*/
+
+/*function start(){
+  document.getElementById("amount").addEventListener("keypress", addActivityItem, false);
+  }
+
+function addActivityItem(){
+  //option is selected
+  alert("yeah");
+}*/
+/*document.addEventListener('turbolinks:load', function(){
+  document.getElementById("amount").addEventListener("keypress", () => {
+    document.getElementById('amount').addEventListener('submit', function(event) {
+      let [result] = event.detail
+      document.getElementById('result').value = result.value
+    })
+  })
+})*/
